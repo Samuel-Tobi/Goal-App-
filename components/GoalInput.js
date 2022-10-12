412497c3868b8cd7584126da9b7720c48454bd53
samuel-tobi <samueltobi52@gmail.com>
2022-10-12 23:41:34 +0100

    Created Goal inputs file and made

---
+++ components/GoalInput.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+export default function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Course goal!"
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add goal" onPress={addGoalHandler} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center ",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    // borderBottom: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,16 +1,20 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function GoalItem(props) {
   return (
-    <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
-    </View>
+    <Pressable onPress={props.onDelete.bind(this, props.id)}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   goalItem: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
     margin: 8,
     padding: 8,
     borderRadius: 6,
